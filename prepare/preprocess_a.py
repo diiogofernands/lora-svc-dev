a597c191d2f322df5e145632d176c42dd3b9a72a
Diogo Fernandes <diogo_fernandes@discente.ufg.br>
2023-06-03 11:01:47 -0300

    Update preprocess_a.py

--- prepare/preprocess_a.py
+++ prepare/preprocess_a.py
@@ -3,14 +3,21 @@ import librosa
 import argparse
 import numpy as np
 
+import multiprocessing
+
 from scipy.io import wavfile
 
 
-def resample_wave(wav_in, wav_out, sample_rate):
-    wav, _ = librosa.load(wav_in, sr=sample_rate)
-    wav = wav / np.abs(wav).max() * 0.6
-    wav = wav / max(0.01, np.max(np.abs(wav))) * 32767 * 0.6
-    wavfile.write(wav_out, sample_rate, wav.astype(np.int16))
+def resample_wave(paths):
+    try:
+        wav_in, wav_out, sample_rate = paths[0], paths[1], paths[2]
+        wav, _ = librosa.load(wav_in, sr=sample_rate)
+        wav = wav / np.abs(wav).max() * 0.6
+        wav = wav / max(0.01, np.max(np.abs(wav))) * 32767 * 0.6
+        wavfile.write(wav_out, sample_rate, wav.astype(np.int16))
+    except:
+        print(paths)
+        exit()
 
 
 if __name__ == "__main__":
@@ -29,6 +36,7 @@ if __name__ == "__main__":
 
     assert args.sr == 16000 or args.sr == 48000
 
+    paths = list()
     for spks in os.listdir(wavPath):
         if os.path.isdir(f"./{wavPath}/{spks}"):
             os.makedirs(f"./{outPath}/{spks}")
@@ -36,9 +44,23 @@ if __name__ == "__main__":
             for file in os.listdir(f"./{wavPath}/{spks}"):
                 if file.endswith(".wav"):
                     file = file[:-4]
-                    resample_wave(f"{wavPath}/{spks}/{file}.wav", f"{outPath}/{spks}/{file}.wav", args.sr)
+                    path = list()
+                    path.append(f"{wavPath}/{spks}/{file}.wav")
+                    path.append(f"{outPath}/{spks}/{file}.wav")
+                    path.append(args.sr)
+                    paths.append(path)
+                    #resample_wave(f"{wavPath}/{spks}/{file}.wav", f"{outPath}/{spks}/{file}.wav", args.sr)
         else:
             file = spks
             if file.endswith(".wav"):
                 file = file[:-4]
-                resample_wave(f"{wavPath}/{file}.wav", f"{outPath}/{file}.wav", args.sr)
+                path = list()
+                path.append(f"{wavPath}/{spks}/{file}.wav")
+                path.append(f"{outPath}/{spks}/{file}.wav")
+                path.append(args.sr)
+                paths.append(path)
+                #resample_wave(f"{wavPath}/{file}.wav", f"{outPath}/{file}.wav", args.sr)
+                
+    pool_obj = multiprocessing.Pool()
+    ans = pool_obj.map(resample_wave, paths)
+    pool_obj.close()
